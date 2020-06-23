8892d4e61def42176d23c27583e654018cc949a4
ronenniv <ronen.niv@gmail.com>
2020-06-23 15:25:11 -0400

    remove app.run

--- run.py
+++ run.py
@@ -2,9 +2,6 @@ from app import app
 
 from db import db
 
-# to be used for Heroko to run by uwsgi
-db.init_app(app)
-
 # SQLAlchemy will create the tables just before the first request
 @app.before_first_request
 def create_tables():
