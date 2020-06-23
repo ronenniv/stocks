830a058ecad5abb1c3ce95c065ef7c3264d7272d
ronenniv <ronen.niv@gmail.com>
2020-06-22 23:16:22 -0400

    comment app.run

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from db import db
 
 db.init_app(app)
 
-app.run(port=5000, debug=True)
+#app.run(port=5000, debug=True)
 
 # SQLAlchemy will create the tables just before the first request
 @app.before_first_request
